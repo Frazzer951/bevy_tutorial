c92de53b4f25ce548eeadc57b4f316a3509ecd81
Luke Eltiste <luke343279@gmail.com>
2022-05-29 16:43:39 -0700

    move player spawn to own file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use bevy::prelude::*;
+use player::PlayerPlugin;
+
+mod player;
 
 // Asset Constants
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (144., 75.);
 
 const SPRITE_SCALE: f32 = 0.5;
+
+// Resources
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
+struct GameTextures {
+    player: Handle<Image>,
+}
 
 fn main() {
     App::new()
@@ -16,6 +29,7 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup_system)
         .run()
 }
@@ -30,20 +44,18 @@ fn setup_system(
 
     // capture window size
     let window = windows.get_primary_mut().unwrap();
-    let (win_l, win_h) = (window.width(), window.height());
+    let (win_w, win_h) = (window.width(), window.height());
 
     // position window (for tutorial)
-    window.set_position(IVec2::new(3200, 170));
+    window.set_position(IVec2::new(1300, 5));
 
-    // add player
-    let bottom = -win_h / 2.;
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load(PLAYER_SPRITE),
-        transform: Transform {
-            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
-            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+    // add WinSize resource
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
+
+    // add GameTextures resource
+    let game_textures = GameTextures {
+        player: asset_server.load(PLAYER_SPRITE),
+    };
+    commands.insert_resource(game_textures);
 }
