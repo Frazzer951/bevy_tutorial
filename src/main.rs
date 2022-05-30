f285435ca9d3b33debc551d400e76b8d95cecc9b
Luke Eltiste <luke343279@gmail.com>
2022-05-29 17:19:31 -0700

    add laser spawns

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,72 @@
+#![allow(unused)]
+
 use bevy::prelude::*;
 use player::PlayerPlugin;
 
+mod components;
 mod player;
 
 // Asset Constants
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (144., 75.);
+const PLAYER_LASER_SPRITE: &str = "laser_a_01.png";
+const PLAYER_LASER_SIZE: (f32, f32) = (9., 54.);
 
 const SPRITE_SCALE: f32 = 0.5;
 
+// Game Constants
+const TIME_STEP: f32 = 1. / 60.;
+const BASE_SPEED: f32 = 500.;
+
 // Resources
 pub struct WinSize {
-    pub w: f32,
-    pub h: f32,
+	pub w: f32,
+	pub h: f32,
 }
 
 struct GameTextures {
-    player: Handle<Image>,
+	player: Handle<Image>,
+	player_laser: Handle<Image>,
 }
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
-        .insert_resource(WindowDescriptor {
-            title: "Rust Invaders!".to_string(),
-            width: 598.0,
-            height: 676.0,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_plugin(PlayerPlugin)
-        .add_startup_system(setup_system)
-        .run()
+	App::new()
+		.insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+		.insert_resource(WindowDescriptor {
+			title: "Rust Invaders!".to_string(),
+			width: 598.0,
+			height: 676.0,
+			..Default::default()
+		})
+		.add_plugins(DefaultPlugins)
+		.add_plugin(PlayerPlugin)
+		.add_startup_system(setup_system)
+		.run()
 }
 
 fn setup_system(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut windows: ResMut<Windows>,
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+	mut windows: ResMut<Windows>,
 ) {
-    // camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+	// camera
+	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    // capture window size
-    let window = windows.get_primary_mut().unwrap();
-    let (win_w, win_h) = (window.width(), window.height());
+	// capture window size
+	let window = windows.get_primary_mut().unwrap();
+	let (win_w, win_h) = (window.width(), window.height());
 
-    // position window (for tutorial)
-    window.set_position(IVec2::new(1300, 5));
+	// position window (for tutorial)
+	window.set_position(IVec2::new(1300, 5));
 
-    // add WinSize resource
-    let win_size = WinSize { w: win_w, h: win_h };
-    commands.insert_resource(win_size);
+	// add WinSize resource
+	let win_size = WinSize { w: win_w, h: win_h };
+	commands.insert_resource(win_size);
 
-    // add GameTextures resource
-    let game_textures = GameTextures {
-        player: asset_server.load(PLAYER_SPRITE),
-    };
-    commands.insert_resource(game_textures);
+	// add GameTextures resource
+	let game_textures = GameTextures {
+		player: asset_server.load(PLAYER_SPRITE),
+		player_laser: asset_server.load(PLAYER_LASER_SPRITE),
+	};
+	commands.insert_resource(game_textures);
 }
--- src/player.rs
+++ src/player.rs
@@ -1,29 +1,88 @@
 use bevy::prelude::*;
 
-use crate::{GameTextures, WinSize, PLAYER_SIZE, SPRITE_SCALE};
+use crate::{
+	components::Player, components::Velocity, GameTextures, WinSize, BASE_SPEED, PLAYER_SIZE,
+	SPRITE_SCALE, TIME_STEP,
+};
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system);
-    }
+	fn build(&self, app: &mut App) {
+		app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
+			.add_system(player_movement_system)
+			.add_system(player_keyboard_event_system)
+			.add_system(player_fire_system);
+	}
 }
 
 fn player_spawn_system(
-    mut commands: Commands,
-    game_textures: Res<GameTextures>,
-    win_size: Res<WinSize>,
+	mut commands: Commands,
+	game_textures: Res<GameTextures>,
+	win_size: Res<WinSize>,
 ) {
-    // add player
-    let bottom = -win_size.h / 2.;
-    commands.spawn_bundle(SpriteBundle {
-        texture: game_textures.player.clone(),
-        transform: Transform {
-            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
-            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+	// add player
+	let bottom = -win_size.h / 2.;
+	commands
+		.spawn_bundle(SpriteBundle {
+			texture: game_textures.player.clone(),
+			transform: Transform {
+				translation: Vec3::new(
+					0.,
+					bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5.,
+					10.,
+				),
+				scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+				..Default::default()
+			},
+			..Default::default()
+		})
+		.insert(Player)
+		.insert(Velocity { x: 0., y: 0. });
+}
+
+fn player_fire_system(
+	mut commands: Commands,
+	kb: Res<Input<KeyCode>>,
+	game_textures: Res<GameTextures>,
+	query: Query<&Transform, With<Player>>,
+) {
+	if let Ok(player_tf) = query.get_single() {
+		if kb.just_pressed(KeyCode::Space) {
+			let (x, y) = (player_tf.translation.x, player_tf.translation.y);
+
+			commands.spawn_bundle(SpriteBundle {
+				texture: game_textures.player_laser.clone(),
+				transform: Transform {
+					translation: Vec3::new(x, y, 0.),
+					scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+					..Default::default()
+				},
+				..Default::default()
+			});
+		}
+	}
+}
+
+fn player_keyboard_event_system(
+	kb: Res<Input<KeyCode>>,
+	mut query: Query<&mut Velocity, With<Player>>,
+) {
+	if let Ok(mut velocity) = query.get_single_mut() {
+		velocity.x = if kb.pressed(KeyCode::Left) {
+			-1.
+		} else if kb.pressed(KeyCode::Right) {
+			1.
+		} else {
+			0.
+		};
+	}
+}
+
+fn player_movement_system(mut query: Query<(&Velocity, &mut Transform), With<Player>>) {
+	for (velocity, mut transform) in query.iter_mut() {
+		let translation = &mut transform.translation;
+		translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+		translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+	}
 }
