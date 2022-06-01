8d70605e5904d8fcf255d7c6c9ea408f3e1dea9d
Luke Eltiste <luke343279@gmail.com>
2022-06-01 15:15:36 -0700

    Add enemy spawns

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![allow(unused)]
 
 use bevy::prelude::*;
-use player::PlayerPlugin;
+
+use crate::components::{Movable, Velocity};
+use crate::enemy::EnemyPlugin;
+use crate::player::PlayerPlugin;
 
 mod components;
+mod enemy;
 mod player;
 
 // Asset Constants
@@ -11,6 +15,11 @@ const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (144., 75.);
 const PLAYER_LASER_SPRITE: &str = "laser_a_01.png";
 const PLAYER_LASER_SIZE: (f32, f32) = (9., 54.);
+
+const ENEMY_SPRITE: &str = "enemy_a_01.png";
+const ENEMY_SIZE: (f32, f32) = (144., 75.);
+const ENEMY_LASER_SPRITE: &str = "laser_b_01.png";
+const ENEMY_LASER_SIZE: (f32, f32) = (17., 55.);
 
 const SPRITE_SCALE: f32 = 0.5;
 
@@ -27,6 +36,8 @@ pub struct WinSize {
 struct GameTextures {
 	player: Handle<Image>,
 	player_laser: Handle<Image>,
+	enemy: Handle<Image>,
+	enemy_laser: Handle<Image>,
 }
 
 fn main() {
@@ -40,7 +51,9 @@ fn main() {
 		})
 		.add_plugins(DefaultPlugins)
 		.add_plugin(PlayerPlugin)
+		.add_plugin(EnemyPlugin)
 		.add_startup_system(setup_system)
+		.add_system(movable_system)
 		.run()
 }
 
@@ -67,6 +80,33 @@ fn setup_system(
 	let game_textures = GameTextures {
 		player: asset_server.load(PLAYER_SPRITE),
 		player_laser: asset_server.load(PLAYER_LASER_SPRITE),
+		enemy: asset_server.load(ENEMY_SPRITE),
+		enemy_laser: asset_server.load(ENEMY_LASER_SPRITE),
 	};
 	commands.insert_resource(game_textures);
+}
+
+fn movable_system(
+	mut commands: Commands,
+	win_size: Res<WinSize>,
+	mut query: Query<(Entity, &Velocity, &mut Transform, &Movable)>,
+) {
+	for (entity, velocity, mut transform, movable) in query.iter_mut() {
+		let translation = &mut transform.translation;
+		translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+		translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+
+		if movable.auto_despawn {
+			// despawn when out of screen
+			const MARGIN: f32 = 200.;
+			if translation.y > win_size.h / 2. + MARGIN
+				|| translation.y < -win_size.h / 2. - MARGIN
+				|| translation.x > win_size.w / 2. + MARGIN
+				|| translation.x < -win_size.w / 2. - MARGIN
+			{
+				println!("->> despawn {entity:?}");
+				commands.entity(entity).despawn();
+			}
+		}
+	}
 }
